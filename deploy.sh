3bff8baaa0bb83c72f39925a20676faab60f3f11
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2015-04-27 22:44:12 +0700

    change commit message & remote url in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git add public
-git commit -m 'update gh-pages'
-git subtree push --prefix=public origin gh-pages
+git commit -m 'update blog'
+git subtree push --prefix=public git@github.com:wingyplus/wingyplus.github.io.git master
